aa06c5d23f756e6a066002078dc270ecb0ef2b64
cbazil114 <113719887+cbazil114@users.noreply.github.com>
2023-03-14 17:07:26 -0400

    Fixed saved books, remove book working

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -53,8 +53,8 @@ const resolvers = {
         },
     removeBook: async (parent, {bookId}, context) => {
         if (context.user) {
-            return Book.findOneAndUpdate(
-              { _id: bookId },
+            return User.findOneAndUpdate(
+              { _id: context.user._id },
               {
                 $pull: {
                   savedBooks: {
